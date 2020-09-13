e974703cff36ab185be829d06f7627dd0450f5ce
Jonifwun <jonathanphilipday@gmail.com>
2020-09-13 19:14:04 -0400

    add modal skeleton for sign up

--- src/App.js
+++ src/App.js
@@ -2,24 +2,77 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import Post from './Components/Post'
 import { db } from './firebase'
+import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, Card } from '@material-ui/core';
 
 function App() {
+  
   const [posts, setPosts] = useState([]);
+  const [openModal, setOpenModal] = useState(false)
+  const [modalStyle] = useState(getModalStyle)
 
   useEffect(() => {
     db.collection('posts').onSnapshot(snapshot => {
-      setPosts(snapshot.docs.map(doc => doc.data()))
+      setPosts(snapshot.docs.map(doc => ({
+        id: doc.id,
+        post: doc.data()
+      }
+      )))
     })  
   }, [])
 
+  function getModalStyle() {
+    const top = 50
+    const left = 50
+  
+    return {
+      top: `${top}%`,
+      left: `${left}%`,
+      transform: `translate(-${top}%, -${left}%)`,
+    };
+  }
+  
+  const useStyles = makeStyles((theme) => ({
+    paper: {
+      position: 'absolute',
+      width: 400,
+      backgroundColor: '#164B61',
+      color: '#FFF',
+      borderRadius: 5,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+   
+  const classes = useStyles()
+
   return (
     <div className="App">
+      <Modal
+        open={ openModal }
+        onClose={() => setOpenModal(false)}
+      >     
+          <Card>
+              <div style={ modalStyle } className={ classes.paper }>
+                <h2 id="simple-modal-title">Sign Up</h2>
+                <p id="simple-modal-description">
+                  Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+                </p>
+              </div>
+          </Card>
+           
+  
+      </Modal>
+
+
       <div className="header">
         <img src="logo.png" alt="logo" className="headerLogo"></img>
+        <Button onClick={() => setOpenModal(true)}>Sign Up</Button>
       </div>
       {
-        posts.map(post => (
-          <Post username={ post.username } caption={ post.caption } imgsrc={ post.imgsrc }/>
+        posts.map(({ post, id }) => (
+          <Post username={ post.username } key={ id } caption={ post.caption } imgsrc={ post.imgsrc }/>
         ))
         
       }
