ddc97708cbb05a9e4acc05df31486aef32404a4d
Jonifwun <jonathanphilipday@gmail.com>
2020-09-28 18:13:01 -0400

    begin search bar implementation

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,35 +1,36 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { Button, Card } from '@material-ui/core'
+import { Card } from '@material-ui/core'
 import Bio from './Bio'
 import UserCard from './UserCard'
 import { UserContext } from '../Contexts/UserContext'
 import Group from './Group'
 import { db } from '../firebase'
+import ProfilePic from './ProfilePic'
 
 
 function Profile() {
+
+    const user = useContext(UserContext)
     
     const [researchGroup, setResearchGroup] = useState({})
-    const user = useContext(UserContext)
-    console.log('Here is:', user)
+    const [searchValue, setSearchValue] = useState('')
+    
+    const handleChange = (e) => {
+        e.preventDefault()
+        setSearchValue(e.target.value)
+    }
 
-   
-    useEffect(() => {
-        //Match the users id to any groups
-
+    useEffect(() => { 
         try {
+             //Match the users id to any groups
             db.collection('researchgroups').where('groupmembers', 'array-contains', user.uid).get()
             .then(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 // doc.data() is never undefined for query doc snapshots
                 console.log(doc.id, " => ", doc.data())
+                //set the research group to state
                 const res = doc.data()
                 setResearchGroup(res)
-
-                // db.collection('researchgroups').doc(doc.id).update({
-                //     groupmembers: firebase.firestore.FieldValue.arrayUnion(user.uid)
-                // })
-                
             });
         })
         .catch(function(error) {
@@ -41,7 +42,9 @@ function Profile() {
         
     }, [user])
 
-    console.log(researchGroup.groupmembers)
+    // const filteredCollaborators = collaborators.filter(user => 
+    //         user.displayName.toLowercase().includes(searchValue.toLowerCase())
+    //     )
 
     const buttonStyle = { color: '#FFF', backgroundColor: '#019CDD', margin: '25px'}
 
@@ -49,10 +52,8 @@ function Profile() {
         <div>
             <Card style={{margin: '100px 30px 40px', backgroundColor: '#0c3141', color: '#FFF', padding: '25px'}}>
                 <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
-                    <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
-                        <img src="profile.jpg" alt="profilepicture" style={{width: '160px', height: "160px", borderRadius: '80px'}}></img>
-                        <Button style={ buttonStyle } >Update Profile Picture</Button>
-                    </div>
+
+                <ProfilePic buttonStyle={ buttonStyle }/>    
                 <Bio />
                 <Group buttonStyle={ buttonStyle } researchGroup={ researchGroup }/>
 
@@ -60,15 +61,23 @@ function Profile() {
                 </div>
             </Card>
             <Card style={{margin: '0 30px 30px', backgroundColor: '#0c3141', color: '#FFF', padding: '25px'}}>
-                <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around'}}>
+                <div style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+                <input type='search' onChange={ handleChange } value= { searchValue } placeholder='Search Collaborators' style={{marginBottom: '25px'}}></input>
+                
+                    { researchGroup?.groupmembers ?
+                    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'space-around', width: '100%'}}>
 
-                    { researchGroup?.groupmembers ? 
-                        researchGroup.groupmembers.map((member) => (
-                            <UserCard key={member.id} user={member}/>
-                        ))
-                        : null
-                    }
-                    
+                        { 
+                            researchGroup.groupmembers.map((member) => (
+                                <div>
+                                <UserCard key={member.id} user={member}/>
+                                </div>
+                            ))
+                        }
+                        </div> 
+                        : null   
+         
+                    }  
 
                 </div>
             </Card>
