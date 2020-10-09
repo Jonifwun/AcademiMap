488d23a28755e229c3e4fc037684b7f53e6e25f4
Jonifwun <jonathanphilipday@gmail.com>
2020-10-09 18:34:49 -0400

    Add passcode reveal/hide button and display

--- src/Components/Group.js
+++ src/Components/Group.js
@@ -1,12 +1,13 @@
 import { Button, Card, Modal } from '@material-ui/core'
-import React, { useState } from 'react'
+import React, { useState, memo } from 'react'
 import JoinGroupForm from './JoinGroupForm'
 import CreateGroupForm from './CreateGroupForm'
 import { makeStyles } from '@material-ui/core/styles';
 
 function Group({ buttonStyle, researchGroup }) {
-
+   
     const [joinGroup, setJoinGroup] = useState(true)
+    const [passcodeDisplay, setPasscodeDisplay] = useState(false)
     const [openModal, setOpenModal] = useState(false)
     const [modalStyle] = useState(getModalStyle)
 
@@ -21,27 +22,19 @@ function Group({ buttonStyle, researchGroup }) {
         };
       };
       
-      const useStyles = makeStyles((theme) => ({
-        paper: {
-          position: 'absolute',
-          width: 400,
-          backgroundColor: '#164B61',
-          color: '#FFF',
-          borderRadius: 5,
-          boxShadow: theme.shadows[5],
-          padding: theme.spacing(2, 4, 3)
-        },
-      }));
+        const useStyles = makeStyles((theme) => ({
+            paper: {
+            position: 'absolute',
+            width: 400,
+            backgroundColor: '#164B61',
+            color: '#FFF',
+            borderRadius: 5,
+            boxShadow: theme.shadows[5],
+            padding: theme.spacing(2, 4, 3)
+            },
+        }));
        
-      const classes = useStyles();
-
-    // const handleJoinGroup = () => {
-
-    // }
-
-    // const handleCreateGroup = () => {
-
-    // }
+        const classes = useStyles();
 
     const groupStyles = {
         display: 'flex',
@@ -49,7 +42,7 @@ function Group({ buttonStyle, researchGroup }) {
         alignItems: "center",
         backgroundColor: '#164B61',
         color: '#FFF',
-        padding: '15px 35px'
+        padding: '15px 30px'
     }
 
     return (
@@ -78,11 +71,27 @@ function Group({ buttonStyle, researchGroup }) {
                     <h3>Research Group:</h3>
                     <h5 style={{margin: '20px'}}><em>{researchGroup.groupName}</em></h5>
                     <p style={{width: '300px', textAlign: 'justify'}}>
-                        Updates: 
+                        Updates:
+                        {//HAVE THIS AS EDIT IN PLACE TEXT TOO (IF GROUP LEADER) - DYNAMICALLY SHOWN 
+}
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
                         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                         Diam quam nulla porttitor massa id neque aliquam.                 
                     </p>             
+                </div>
+                <hr style={{width: '100%'}}></hr>
+                <div style={{display: 'flex'}}>
+                    <Button style={ buttonStyle } onClick={() => setPasscodeDisplay(!passcodeDisplay)}>
+                        {passcodeDisplay ? 'Hide Passcode' : 'Show Passcode'}
+                    </Button>
+                    {passcodeDisplay ? 
+                        <div style={{display: 'flex', flexDirection: 'column', marginTop: '5px',backgroundColor: '#0D3140', borderRadius: '5px', padding:'7px', fontSize: '13px', alignItems: 'center'}}>
+                            <p><em>Group Passcode: { researchGroup.groupPasscode }</em></p>
+                            <br></br>
+                            <p>Give this passcode to any user<br></br> 
+                                you wish to join the group</p>
+                        </div> 
+                        : null}                    
                 </div>
             </Card>
             : 
@@ -93,20 +102,14 @@ function Group({ buttonStyle, researchGroup }) {
                     setOpenModal(true)
                     setJoinGroup(true)
                 }}>Join Group</Button>
-
                 <Button style={ buttonStyle } onClick={() => {
                     setOpenModal(true)
                     setJoinGroup(false)
                 }}>Create Group</Button>
-
-                {
-                    // For Create Group. need logic to create research group by name and give a reference value for joining. 
-
-                }
             </Card>
             }  
         </div>
     )
 }
 
-export default Group
+export const MemoizedGroup = memo(Group)
