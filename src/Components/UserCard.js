9e7756c72bea0fd3ea2a1ab6dc6a1ceb0fffc4db
Jonifwun <jonathanphilipday@gmail.com>
2020-10-13 18:42:46 -0400

    add react router links to UserCard username and avatar

--- src/Components/UserCard.js
+++ src/Components/UserCard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState, memo } from 'react'
 import { Card } from '@material-ui/core'
 import { db } from '../firebase'
+import { Link } from 'react-router-dom'
 
 export function UserCard({ username }) {
 
@@ -20,8 +21,17 @@ export function UserCard({ username }) {
     return (
         <div>
             <Card style={{display: 'flex', flexDirection: "column", alignItems: "center", backgroundColor: '#164B61', color: '#FFF', padding: '15px 35px'}}>
-                <h5>{ userData.username }</h5>                    
-                <img src={ userData.photoURL } alt="profilepicture" style={{width: '80px', height: "80px", borderRadius: '80px', border: '4px solid #019CDD' , margin: '15px'}}></img>
+                <h5>
+                    <Link 
+                    to={`/users/${userData.username}`} 
+                    style={{textDecoration: "none", color: '#FFF'}}
+                    >
+                    { userData.username }
+                    </Link>
+                </h5>
+                <Link to={`/users/${userData.username}`}>
+                    <img src={ userData.photoURL } alt="profilepicture" style={{width: '80px', height: "80px", borderRadius: '80px', border: '4px solid #019CDD' , margin: '15px'}}></img>
+                </Link>                    
 
             </Card>
         </div>
