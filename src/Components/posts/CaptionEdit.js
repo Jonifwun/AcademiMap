db64c73619dee85c8974e043bce04c7470739c4a
Jonifwun <jonathanphilipday@gmail.com>
2020-10-25 20:29:14 -0400

    fix modal bug in updateCaption

--- src/Components/posts/CaptionEdit.js
+++ src/Components/posts/CaptionEdit.js
@@ -1,6 +1,5 @@
 import { Button, FormControl, Input } from '@material-ui/core'
 import React, { useState } from 'react'
-import { db } from '../../firebase'
 import { updateCaption } from './updateCaption'
 
 
@@ -9,40 +8,8 @@ const CaptionEdit = ({ setOpenModal, username, caption, postID, researchGroupID 
     const [captionText, setCaptionText] = useState(caption)
 
     const handleUpdate = (e) => {
-    e.preventDefault();
-    
-    updateCaption([{type: 'users', id: username}, {type: 'researchgroups', id: researchGroupID}], postID, captionText)
-    
-
-        db.collection('users')
-          .doc(username)
-          .collection('posts')
-          .doc(postID)
-          .update({
-              caption: captionText
-          })
-          .then(() => {
-            console.log("Caption successfully updated!");
-            setOpenModal(false)
-          })
-          .catch(err => {
-            console.error("Error updating caption: ", err);
-          })
-          
-        db.collection('researchgroups')
-          .doc(researchGroupID)
-          .collection('posts')
-          .doc(postID)
-          .update({
-              caption: captionText
-          })
-          .then(() => {
-            console.log("Caption successfully updated!");
-            setOpenModal(false)
-          })
-          .catch(err => {
-            console.error("Error updating caption: ", err);
-          })
+      e.preventDefault();
+      updateCaption([{type: 'users', id: username}, {type: 'researchgroups', id: researchGroupID}], postID, captionText, setOpenModal)
     }
 
     return (
